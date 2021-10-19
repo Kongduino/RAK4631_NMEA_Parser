3ab61a49b2d88c89b6e80ea5f5acbfdef4f645d1
Kongduino <dbvic@me.com>
2021-10-19 11:58:07 +0800

    First commit

--- RAK4631_NMEA_Parser.ino
+++ RAK4631_NMEA_Parser.ino
@@ -24,31 +24,14 @@ vector<string> userStrings;
 char UTC[7] = {0};
 uint8_t SIV = 0;
 
-struct RawDegrees {
-    uint16_t deg;
-    uint32_t billionths;
-    bool negative;
-  public:
-    RawDegrees() : deg(0), billionths(0), negative(false) {}
-};
+float latitude, longitude;
 
-RawDegrees latitude, longitude;
-
-void parseDegrees(const char *term, RawDegrees &deg) {
-  uint32_t leftOfDecimal = (uint32_t)atol(term);
-  uint16_t minutes = (uint16_t)(leftOfDecimal % 100);
-  uint32_t multiplier = 10000000UL;
-  uint32_t tenMillionthsOfMinutes = minutes * multiplier;
-  deg.deg = (int16_t)(leftOfDecimal / 100);
-  while (isdigit(*term))
-    ++term;
-  if (*term == '.')
-    while (isdigit(*++term)) {
-      multiplier /= 10;
-      tenMillionthsOfMinutes += (*term - '0') * multiplier;
-    }
-  deg.billionths = (5 * tenMillionthsOfMinutes + 1) / 3;
-  deg.negative = false;
+float parseDegrees(const char *term) {
+  float value = (float)(atof(term) / 100.0);
+  uint16_t left = (uint16_t)value;
+  value = (value - left) * 1.66666666666666;
+  value += left;
+  return value;
 }
 
 vector<string> parseNMEA(string nmea) {
@@ -80,10 +63,22 @@ void parseGPRMC(vector<string>result) {
     Serial.print(result.at(1).substr(2, 2).c_str());
     Serial.print(":");
     Serial.println(result.at(1).substr(4, 2).c_str());
+    sprintf(buffer, "%s:%s:%s UTC", result.at(1).substr(0, 2).c_str(), result.at(1).substr(2, 2).c_str(), result.at(1).substr(4, 2).c_str());
+    Serial.println(buffer);
   }
-  if (result.at(2) == "V") {
-    Serial.println("Invalid fix!");
-  } else Serial.println("Valid fix!");
+  // if (result.at(2) == "V") Serial.println("Invalid fix!");
+  // else Serial.println("Valid fix!");
+  if (result.at(3) != "") {
+    float newLatitude, newLongitude;
+    newLatitude = parseDegrees(result.at(3).c_str());
+    newLongitude = parseDegrees(result.at(5).c_str());
+    if (newLatitude != latitude || newLongitude != longitude) {
+      latitude = newLatitude;
+      longitude = newLongitude;
+      sprintf(buffer, "Coordinates: %3.8f %c, %3.8f %c\n", latitude, result.at(4).c_str()[0], longitude, result.at(6).c_str()[0]);
+      Serial.print(buffer);
+    }
+  }
 }
 
 void parseGPGGA(vector<string>result) {
@@ -95,23 +90,83 @@ void parseGPGGA(vector<string>result) {
     Serial.print(":");
     Serial.println(result.at(1).substr(4, 2).c_str());
   }
-  if (result.at(6) == "0") {
-    Serial.println("Invalid fix!");
-  } else Serial.println("Valid fix!");
+  //  if (result.at(6) == "0") Serial.println("Invalid fix!");
+  //  else Serial.println("Valid fix!");
   if (result.at(2) != "") {
-    parseDegrees(result.at(2).c_str(), latitude);
-    Serial.print("Coordinates: ");
-    Serial.print(latitude.deg);
-    Serial.write('.');
-    Serial.print(latitude.billionths);
-    Serial.print(result.at(3).c_str());
-    Serial.print(", ");
-    parseDegrees(result.at(4).c_str(), longitude);
-    Serial.print(longitude.deg);
-    Serial.write('.');
-    Serial.print(longitude.billionths);
-    Serial.println(result.at(5).c_str());
+    latitude = parseDegrees(result.at(2).c_str());
+    longitude = parseDegrees(result.at(4).c_str());
+    sprintf(buffer, "Coordinates: %3.8f %c, %3.8f %c\n", latitude, result.at(3).c_str()[0], longitude, result.at(5).c_str()[0]);
+    Serial.print(buffer);
   }
+}
+
+void parseGPGLL(vector<string>result) {
+  if (result.at(1) != "") {
+    latitude = parseDegrees(result.at(1).c_str());
+    longitude = parseDegrees(result.at(3).c_str());
+    sprintf(buffer, "Coordinates: %3.8f %c, %3.8f %c\n", latitude, result.at(2).c_str()[0], longitude, result.at(4).c_str()[0]);
+    Serial.print(buffer);
+  }
+}
+
+void parseGPGSV(vector<string>result) {
+  if (result.at(1) != "") {
+    uint8_t newSIV = atoi(result.at(3).c_str());
+    if (SIV != newSIV) {
+      sprintf(buffer, "Message %s / %s. SIV: %s\n", result.at(2).c_str(), result.at(1).c_str(), result.at(3).c_str());
+      Serial.print(buffer);
+      SIV = newSIV;
+    }
+  }
+}
+
+void parseGPTXT(vector<string>result) {
+  //$GPTXT, 01, 01, 02, ANTSTATUS = INIT
+  if (result.at(1) != "") {
+    sprintf(buffer, " . Message %s / %s. Severity: %s\n . Message text: %s\n",
+            result.at(2).c_str(), result.at(1).c_str(), result.at(3).c_str(), result.at(4).c_str(), result.at(5).c_str());
+    Serial.print(buffer);
+  }
+}
+
+void parseGPVTG(vector<string>result) {
+  Serial.println("Track Made Good and Ground Speed.");
+  if (result.at(1) != "") {
+    sprintf(buffer, " . True track made good %s [%s].\n", result.at(1).c_str(), result.at(2).c_str());
+    Serial.print(buffer);
+  }
+  if (result.at(3) != "") {
+    sprintf(buffer, " . Magnetic track made good %s [%s].\n", result.at(3).c_str(), result.at(4).c_str());
+    Serial.print(buffer);
+  }
+  if (result.at(5) != "") {
+    sprintf(buffer, " . Speed: %s %s.\n", result.at(5).c_str(), result.at(6).c_str());
+    Serial.print(buffer);
+  }
+  if (result.at(7) != "") {
+    sprintf(buffer, " . Speed: %s %s.\n", result.at(7).c_str(), result.at(8).c_str());
+    Serial.print(buffer);
+  }
+}
+
+void parseGPGSA(vector<string>result) {
+  // $GPGSA,A,3,15,29,23,,,,,,,,,,12.56,11.96,3.81
+  Serial.println("GPS DOP and active satellites");
+  if (result.at(1) == "A") Serial.println(" . Mode: Automatic");
+  else if (result.at(1) == "M") Serial.println(" . Mode: Manual");
+  else Serial.println(" . Mode: ???");
+  if (result.at(2) == "1") {
+    Serial.println(" . Fix not available.");
+    return;
+  } else if (result.at(2) == "2") Serial.println(" . Fix: 2D");
+  else if (result.at(2) == "3") Serial.println(" . Fix: 3D");
+  else {
+    Serial.println(" . Fix: ???");
+    return;
+  }
+  Serial.print(" . PDOP: "); Serial.println(result.at(result.size() - 3).c_str());
+  Serial.print(" . HDOP: "); Serial.println(result.at(result.size() - 2).c_str());
+  Serial.print(" . VDOP: "); Serial.println(result.at(result.size() - 1).c_str());
 }
 
 void setup() {
@@ -125,8 +180,7 @@ void setup() {
     }
   }
   delay(2500);
-  Serial.println("\nGPS Example(I2C)");
-  //  pinMode(WB_IO1, OUTPUT); // SLOT_A SLOT_B
+  Serial.println("\nGPS Example (NMEA Parser)");
   Serial.println("Turning off port");
   pinMode(WB_IO2, OUTPUT); // SLOT_A SLOT_B
   digitalWrite(WB_IO2, 0);
@@ -154,10 +208,7 @@ void loop() {
         c = Serial2.read();
         delay(50);
         string nextLine = string(buffer);
-        //Serial.print("Appending: {");
-        //Serial.print(nextLine.c_str());
-        //Serial.println("}");
-        userStrings.push_back(nextLine);
+        userStrings.push_back(nextLine.substr(0, nextLine.size() - 3));
         waitForDollar = true;
       } else {
         buffer[ix++] = c;
@@ -170,17 +221,26 @@ void loop() {
     if (nextLine.substr(0, 1) != "$") {
       Serial.print("Not an NMEA string!\n>> ");
       Serial.println(nextLine.c_str());
+      return;
     } else {
       vector<string>result = parseNMEA(nextLine);
-      Serial.print("Number of tokens: ");
-      Serial.println(result.size());
       string verb = result.at(0);
       if (verb == "$GPRMC") {
-        Serial.println(nextLine.c_str());
         parseGPRMC(result);
+      } else if (verb == "$GPGSV") {
+        parseGPGSV(result);
       } else if (verb == "$GPGGA") {
-        Serial.println(nextLine.c_str());
         parseGPGGA(result);
+      } else if (verb == "$GPGLL") {
+        parseGPGLL(result);
+      } else if (verb == "$GPGSA") {
+        parseGPGSA(result);
+      } else if (verb == "$GPVTG") {
+        parseGPVTG(result);
+      } else if (verb == "$GPTXT") {
+        parseGPTXT(result);
+      } else {
+        Serial.println(nextLine.c_str());
       }
     }
   }
